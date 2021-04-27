676b51e83c0ebbcf68d60ebcf4fd28f3931040ae
HPavlova <77845327+HPavlova@users.noreply.github.com>
2021-04-27 19:06:20 +0300

    добавляет js

--- src/js/counter.js
+++ src/js/counter.js
@@ -1,15 +1,22 @@
 $(document).ready(function () {
-
-    $('.count').each(function () {
-      $(this).prop('Counter',0).animate({
-      Counter: $(this).text()
-      }, {
-      duration: 1500,
-      easing: 'swing',
-      step: function (now) {
-      $(this).text(Math.ceil(now));
-      }
+  var show = true;
+  var countbox = '.advantages__list';
+  $(window).on('scroll load resize', function () {
+    if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+    var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+    var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+    var w_height = $(window).height(); // Высота окна браузера
+    var d_height = $(document).height(); // Высота всего документа
+    var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+    if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+      $('.count').css('opacity', '1');
+      $('.kg').css('opacity', '1');
+      $('.count').spincrement({
+        thousandSeparator: '',
+        duration: 1200,
       });
-    });
-    
-    });
+
+      show = false;
+    }
+  });
+});
