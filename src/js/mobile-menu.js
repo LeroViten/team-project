cd17db5c1988b66b3af18fb5acc0bc59c5dc2ba0
Valerii Vitenko <valero2313@gmail.com>
2021-04-28 18:23:53 +0300

    usability with burg close on outside tap
    outside tap works but you cannot open mob menu from burg button as it is element in Header :(

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,10 +2,19 @@
     const menuBtnRef = document.querySelector("[data-menu-button]");
     const mobileMenuRef = document.querySelector("[data-menu]");
     const mobileBtnClose = document.querySelector("[data-menu-close]");
-    menuBtnRef.addEventListener("click", () => {
+  
+  menuBtnRef.addEventListener("click", () => {
+      const expanded =
+      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+    mobileMenuRef.classList.toggle("is-open");
+    menuBtnRef.setAttribute("aria-expanded", !expanded);
+    })
+
+    mobileBtnClose.addEventListener('click', () => {
       mobileMenuRef.classList.toggle("is-open");
     });
-    mobileBtnClose.addEventListener("click", () => {
-      mobileMenuRef.classList.toggle("is-open");
-    });
-  })();
+  // $('.header-hero, .products').click(function () {
+  //     mobileMenuRef.classList.remove('is-open'),
+  //       menuBtnRef.classList.remove('is-open');
+  //   });
+})()
